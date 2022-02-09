d084cf3ff49be21987d272fcc9c31eb7c3d43a8b
Seth Onyango <sethi.onyango@gmail.com>
2022-02-09 10:02:47 +0300

    Update file service tests

--- src/test/java/io/falu/services/FilesServiceTests.java
+++ src/test/java/io/falu/services/FilesServiceTests.java
@@ -12,29 +12,59 @@ import io.falu.models.files.links.FileLinksListOptions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 @ExtendWith(MockitoExtension.class)
 public class FilesServiceTests extends BaseApiServiceTests {
 
+    private final File file = File.builder()
+            .fileName("test.png")
+            .type("image/png")
+            .purpose("customer.evaluation")
+            .size(1024)
+            .id("file_123")
+            .build();
+
+    private final FileLink fileLink = FileLink.builder()
+            .fileId("file_123")
+            .created(new Date())
+            .url("https/::example.com/123")
+            .expired(false)
+            .build();
+
+    @Mock
+    private FilesService service;
+
     @Test
     public void test_GetFilesWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
         FileListOptions opt = FileListOptions.builder()
                 .count(1)
                 .build();
 
-        ResourceResponse<File[]> response = service.getFiles(opt, requestOptions);
-        Assertions.assertEquals(200, response.getStatusCode());
-        Assertions.assertNotNull(response.getResource());
+        ResourceResponse<File[]> response = getResourceResponse(200, new File[]{file});
+        when(service.getFiles(opt, requestOptions)).thenReturn(response);
+
+        mockWebServer.enqueue(getMockedResponse(200, new File[]{file}));
+
+        // when
+        ResourceResponse<File[]> resp = service.getFiles(opt, requestOptions);
+        Assertions.assertNotNull(resp);
+        Assertions.assertTrue(response.getResource().length > 0);
     }
 
     @Test
     public void test_FileUploadWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
         FileCreateRequest request = FileCreateRequest.builder()
                 .content(new java.io.File("file.pdf"))
@@ -42,6 +72,13 @@ public class FilesServiceTests extends BaseApiServiceTests {
                 .purpose("customer.signature")
                 .build();
 
+        // given
+        ResourceResponse<File> expectedResponse = getResourceResponse(200, file);
+        when(service.uploadFile(request, requestOptions)).thenReturn(expectedResponse);
+
+        mockWebServer.enqueue(getMockedResponse(200, new File[]{file}));
+
+        // when
         ResourceResponse<File> response = service.uploadFile(request, requestOptions);
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getResource());
@@ -49,8 +86,15 @@ public class FilesServiceTests extends BaseApiServiceTests {
 
     @Test
     public void test_GetFileWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
+        // given
+        ResourceResponse<File> expectedResponse = getResourceResponse(200, file);
+        when(service.getFile("file_123", requestOptions)).thenReturn(expectedResponse);
+
+        mockWebServer.enqueue(getMockedResponse(200, new File[]{file}));
+
+        // when
         ResourceResponse<File> response = service.getFile("file_123", requestOptions);
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getResource());
@@ -58,12 +102,19 @@ public class FilesServiceTests extends BaseApiServiceTests {
 
     @Test
     public void test_GetFileLinksWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
         FileLinksListOptions opt = FileLinksListOptions.builder()
                 .count(1)
                 .build();
 
+        // given
+        ResourceResponse<FileLink[]> expectedResponse = getResourceResponse(200, new FileLink[]{fileLink});
+        when(service.getFileLinks(opt, requestOptions)).thenReturn(expectedResponse);
+
+        mockWebServer.enqueue(getMockedResponse(200, new File[]{file}));
+
+        // when
         ResourceResponse<FileLink[]> response = service.getFileLinks(opt, requestOptions);
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getResource());
@@ -71,12 +122,19 @@ public class FilesServiceTests extends BaseApiServiceTests {
 
     @Test
     public void test_CreateFileLinkWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
         FileLinkCreateRequest request = FileLinkCreateRequest.builder()
                 .fileId("file_123")
                 .build();
 
+        // given
+        ResourceResponse<FileLink> expectedResponse = getResourceResponse(200, fileLink);
+        when(service.createFileLink(request, requestOptions)).thenReturn(expectedResponse);
+
+        mockWebServer.enqueue(getMockedResponse(200, file));
+
+        // when
         ResourceResponse<FileLink> response = service.createFileLink(request, requestOptions);
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getResource());
@@ -84,8 +142,15 @@ public class FilesServiceTests extends BaseApiServiceTests {
 
     @Test
     public void test_GetFileLinkWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
+        // given
+        ResourceResponse<FileLink> expectedResponse = getResourceResponse(200, fileLink);
+        when(service.getFileLink("link_123", requestOptions)).thenReturn(expectedResponse);
+
+        mockWebServer.enqueue(getMockedResponse(200, file));
+
+        // when
         ResourceResponse<FileLink> response = service.getFileLink("link_123", requestOptions);
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getResource());
@@ -93,11 +158,18 @@ public class FilesServiceTests extends BaseApiServiceTests {
 
     @Test
     public void test_UpdateFileLinkWorks() throws IOException {
-        FilesService service = new FilesService(options);
+        service = Mockito.mock(FilesService.class, withSettings().useConstructor(options));
 
         JsonPatchDocument<FileLinkPatchModel> document = new JsonPatchDocument<FileLinkPatchModel>()
                 .replace("description", "cake");
 
+        // given
+        ResourceResponse<FileLink> expectedResponse = getResourceResponse(200, fileLink);
+        when(service.updateFileLink("link_123", document, requestOptions)).thenReturn(expectedResponse);
+
+        mockWebServer.enqueue(getMockedResponse(200, file));
+
+        // when
         ResourceResponse<FileLink> response = service.updateFileLink("link_123", document, requestOptions);
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getResource());
